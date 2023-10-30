e19c998e2aa2b5e22cfa368f4080e0c63d53a217
ritamarieh <116038372+ritamarieh@users.noreply.github.com>
2023-10-29 18:11:58 -0700

    Delete MATLAB Projects/Introduction to Mathematical Programming/Q5i.m

--- MATLAB Projects/Introduction to Mathematical Programming/Q5i.m
+++
@@ -1,8 +0,0 @@
-%Estimation of the integral of int_a^b(x^2)dx
-function fvalue = Q5i(x) 
- fvalue = sin(x)./(cos(x).^2+5.*cos(x)+6); 
- end
-
-%function fvalue = Q5i(x) 
-%fvalue = exp(-x.^2)/(sqrt(2*pi)); 
-%end 
